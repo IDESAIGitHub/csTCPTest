eff196676255c78e0efec4d28d115c0d8f5f3ff1
IDESAIGitHub <rwang@idesai.es>
2023-10-31 18:00:42 +0100

    Split into Server and Client. This is server. Works !!

--- TCPZebra_test/Form1.cs
+++ TCPZebra_test/Form1.cs
@@ -1,3 +1,7 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 namespace TCPZebra_test;
 
 
@@ -6,16 +10,24 @@ namespace TCPZebra_test;
 
 public partial class Form1 : Form
 {
-    private TcpClientExample _client;
+    private TcpListener _server = null;
+    private TcpClient _client;
+
+
+
+    //private TcpClientExample _client;
     public Form1()
     {
         InitializeComponent();
-        InitializeComponent();
-        _client = new TcpClientExample();
+        _server = new TcpListener(IPAddress.Any, 420);
+        _server.Start();
+        _client = _server.AcceptTcpClient();
 
-        _client.ConnectAsync();
+        //_client = new TcpClientExample();
 
-        _client.MessageReceived += Client_MessageReceived;
+        //_client.ConnectAsync();
+
+        //_client.MessageReceived += Client_MessageReceived;
     }
 
     private void Client_MessageReceived(object? sender, string e)
@@ -25,11 +37,19 @@ public partial class Form1 : Form
 
     private void button1_Click(object sender, EventArgs e)
     {
-        _client.Send("TRIGGER\r");
-        //Thread.Sleep(150);
         //_client.Send("TRIGGER\r");
-        //Thread.Sleep(150);
-        //_client.Send("TRIGGER\r");
+
+        if (_client != null)
+        {
+            NetworkStream stream = _client.GetStream();
+            string message = "Hello World \n" + (char) 3;
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+        }
+        else
+        {
+            MessageBox.Show("No client connected");
+        }
     }
 
     private void Form1_Load(object sender, EventArgs e)
@@ -39,6 +59,14 @@ public partial class Form1 : Form
 
     private void Form1_FormClosed(object sender, FormClosedEventArgs e)
     {
-        _client.Disconnect();
+        //_client.Disconnect();
+        if (_server != null)
+        {
+            _server.Stop();
+        }
+        if (_client != null)
+        {
+            _client.Close();
+        }
     }
 }
